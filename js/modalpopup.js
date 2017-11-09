dc182b8a41a16722a8a72fbd6468f9f112477a01
Luke Bateman <bateman.w.luke@gmail.com>
2017-11-09 08:21:50 -0500

    stored projView and ProjInfo vars in main.js to be used for populating landing page

--- js/modalpopup.js
+++ js/modalpopup.js
@@ -5,34 +5,7 @@ $("a[rel*=leanModal]").leanModal({ top : offsetTop, overlay : 0.4, closeButton: 
 
 
 //Creates a matrix variable to store all project info displayed within the modal
-var projInfo = {
-  //Project Organizations
-	'senstaff-org'   : "Design, Innovation, & Society",
-  //Project Title
-  'senstaff-title' : 'Senstaff',
-  //Project Categories
-  'senstaff-cat' : 'Product',
-  //Project subtitle
-	'senstaff-subtitle' : 'I am a subtitle!',
-	//Project problem
-	'senstaff-problem' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-	//Project solution
-	'senstaff-solution' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-	//Project final
-	'senstaff-final' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-	//Additional Info Link
-	'senstaff-link'   : 'https://drive.google.com/open?id=0B32gQGnV1_U-UVdaT0tockxDQUk',
-};
-
-var projView = {
-	'senstaff-team' : ['Luke Bateman', 'Lei Luo', "Sean Alderdice", "Lilit Balagyozyan"],
-	//Project Images
-	'senstaff-img' : ['url(images/projects/senstaff/prototype1.jpg)',
-	'url(images/projects/senstaff/working.jpg)'],
-	//Project Image Description
-  'senstaff-imgTxt' : ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-	'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']
-};
+//Stored variables in main.js to reference for populating the landing page
 
 //Creates a matrix variable to store all the team member info associated with each project
 /*var teamInfo = {
